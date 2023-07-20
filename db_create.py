c1b2b5cbed34a36f39b512f68bc8babbac1a0dd9
Martín Szyszlican <martinsz@gmail.com>
2023-07-20 16:35:06 -0600

    Allow command line token

--- db_create.py
+++ db_create.py
@@ -1,5 +1,7 @@
 from app import db
 from app.models.token_auth import TokenAuth
+import sys 
+ 
 
 print('Drop all')
 db.drop_all()
@@ -9,7 +11,11 @@ db.create_all()
 
 print("Token time")
 token_type = "Bearer"
-token_token = input("Enter token: ")
+
+if (len(sys.argv) > 1):
+    token_token = sys.argv[1]
+else:
+    token_token = input("Enter token: ")
 token = TokenAuth(token_type, token_token)
 token.save()
 
